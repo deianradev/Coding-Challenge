c388889674b313c3d3f0156505fc5099d0879bf7
Deian Radev <deianradev@deians-imac.myfiosgateway.com>
2022-06-26 20:29:16 -0400

    bonus points: add alternate text & title in event click

--- script/script.js
+++ script/script.js
@@ -1,5 +1,10 @@
 const elementPark = document.getElementById("parkButton");
+const elementParkTitle = document.getElementById("parkTitle");
+const elementParkText = document.getElementById("parkText");
+
 const elementMonth = document.getElementById("monthButton");
+const elementMonthTitle = document.getElementById("monthTitle");
+const elementMonthText = document.getElementById("monthText");
 
 /* create two variables to track each visibility state of both buttons separately*/
 let isHidden = false;
@@ -10,16 +15,42 @@ elementPark.innerHTML = '<i class="fa-solid fa-eye-slash"></i> Hide charity'
 elementMonth.innerHTML = '<i class="fa-solid fa-eye-slash"></i> Hide giving plan'
 
 /* toggle functionality to change boolean values true=>false and false=>true */
-const toggleHide = () => isHidden = !isHidden;
+const toggleMonthHide = () => isHidden = !isHidden;
 const toggleParkHide = () => isParkHidden = !isParkHidden;
 
 /* event listener for charity button */
 elementPark.addEventListener("click", function () {
-    let hideShow = (toggleHide() ? '<i class="fa-solid fa-eye"></i> Show' : '<i class="fa-solid fa-eye-slash"></i> Hide')
+    //let hideShow = (toggleMonthHide() ? '<i class="fa-solid fa-eye"></i> Show' : '<i class="fa-solid fa-eye-slash"></i> Hide')
+    let hideShow, title, text;
+
+    if (toggleMonthHide()) {
+        hideShow = '<i class="fa-solid fa-eye"></i> Show';
+        title = 'Another Title';
+        text = 'Sed ut perspiciatis unde omnis iste natus';
+    } else {
+        hideShow = '<i class="fa-solid fa-eye-slash"></i> Hide';
+        title = 'New!';
+        text = 'National Park Trust has been added';
+    }
     elementPark.innerHTML = `${hideShow}  charity`;
+    elementParkTitle.innerHTML = `${title}`;
+    elementParkText.innerHTML = `${text}`;
 });
 /* event listener for park giving plan button */
 elementMonth.addEventListener("click", function () {
-    let hideShow = (toggleParkHide() ? '<i class="fa-solid fa-eye"></i> Show' : '<i class="fa-solid fa-eye-slash"></i> Hide')
+    //let hideShow = (toggleParkHide() ? '<i class="fa-solid fa-eye"></i> Show' : '<i class="fa-solid fa-eye-slash"></i> Hide')
+    let hideShow = '';
+    let title = '';
+    if (toggleParkHide()) {
+        hideShow = '<i class="fa-solid fa-eye"></i> Show';
+        title = 'Alternate Title';
+        text = 'Lorem ipsum dolor sit amet, consectetur elit';
+    } else {
+        hideShow = '<i class="fa-solid fa-eye-slash"></i> Hide';
+        title = '$150/mo';
+        text = 'Next giving plan disbursement occurs on 08/30/20.'
+    }
     elementMonth.innerHTML = `${hideShow}  giving plan`;
+    elementMonthTitle.innerHTML = `${title}`;
+    elementMonthText.innerHTML = `${text}`;
 });
